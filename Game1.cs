5e524a3f6a42ccf8dc711215ae998b81ca04ed55
Micah Ma <701004018@GoTVDSB.ca>
2024-03-21 14:36:20 -0400

    Made changes

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
 {
@@ -10,9 +15,11 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
         private SpriteBatch _spriteBatch;
 
         MouseState mouseState, prevMousestate;
-        KeyboardState keyboardState;
+        KeyboardState keyboardState, prevKeyboardState;
 
         //Textures
+        Texture2D pacmanIntroTexture;
+
         Texture2D pacLeftTexture;
         Texture2D pacRightTexture;
         Texture2D pacUpTexture;
@@ -26,10 +33,10 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
         Rectangle exitRect;
 
         Texture2D barrierTexture;
-        Rectangle barrierRect1, barrierRect2;
-
         Texture2D coinTexture;
-        Rectangle coinRect;
+
+        int pacSpeed, coinUpandDown;
+        float idleSeconds,gametimeSeconds;
 
         enum Screen
         {
@@ -38,6 +45,12 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
             result
         }
         Screen screen;
+
+        List<Rectangle> coins = new List<Rectangle>();
+        List<Rectangle> barriers = new List<Rectangle> ();
+
+        Random gen = new Random ();
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -53,9 +66,12 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
             _graphics.PreferredBackBufferWidth = 640;
             _graphics.PreferredBackBufferHeight = 640;
             _graphics.ApplyChanges();
-
-
-
+            
+            pacSpeed = 3;
+            pacRect = new Rectangle(10, 10, 60, 60);
+            exitRect = new Rectangle(550, 500, 100, 100);
+            coinUpandDown = 0;
+            
             base.Initialize();
         }
 
@@ -64,6 +80,7 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            pacmanIntroTexture = Content.Load<Texture2D>("pacmanIntro");
             // Pacman
             pacDownTexture = Content.Load<Texture2D>("pacdown");
             pacUpTexture = Content.Load<Texture2D>("pacup");
@@ -77,6 +94,14 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
             exitTexture = Content.Load<Texture2D>("hobbit_door");
             // Coin
             coinTexture = Content.Load<Texture2D>("coin");
+
+            coins.Add(new Rectangle(400, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(475, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(200, 300, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(400, 300, coinTexture.Width, coinTexture.Height));
+         
+            barriers.Add(new Rectangle(0, 200, 350, 75));
+            barriers.Add(new Rectangle(450, 200, 350, 75));
         }
 
         protected override void Update(GameTime gameTime)
@@ -85,9 +110,181 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
                 Exit();
 
             // TODO: Add your update logic here
+            prevMousestate = mouseState;
+            prevKeyboardState = keyboardState;
             keyboardState = Keyboard.GetState();
             mouseState = Mouse.GetState();
-            
+            idleSeconds = (float)gameTime.TotalGameTime.TotalSeconds-gametimeSeconds;
+
+            if (screen == Screen.intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Released && prevMousestate.LeftButton == ButtonState.Pressed)
+                {
+                    screen = Screen.pacman;
+                    currentPacTexture = pacSleepTexture;
+                    gametimeSeconds = (float)gameTime.TotalGameTime.TotalSeconds;
+                }
+            }
+            else if (screen == Screen.pacman)
+            {
+                if (keyboardState.IsKeyDown(Keys.W))
+                {
+                    pacRect.Y -= pacSpeed;
+                    currentPacTexture = pacUpTexture;
+                    gametimeSeconds = (float)gameTime.TotalGameTime.TotalSeconds;
+                    foreach (Rectangle barrier in barriers)
+                        if (pacRect.Intersects(barrier))
+                        {
+                            pacRect.Y = barrier.Bottom;
+                        }
+                    if (pacRect.Y <= 0)
+                    {
+                        pacRect.Y = 0;
+                    }
+
+                }
+                if (keyboardState.IsKeyDown(Keys.S))
+                {
+                    pacRect.Y += pacSpeed;
+                    currentPacTexture = pacDownTexture;
+                    gametimeSeconds = (float)gameTime.TotalGameTime.TotalSeconds;
+                    foreach (Rectangle barrier in barriers)
+                        if (pacRect.Intersects(barrier))
+                        {
+                            pacRect.Y = barrier.Top - pacRect.Height;
+                        }
+                    if (pacRect.Bottom >= _graphics.PreferredBackBufferHeight)
+                    {
+                        pacRect.Y = _graphics.PreferredBackBufferHeight - pacRect.Height;
+                    }
+                }
+                if (keyboardState.IsKeyDown(Keys.A))
+                {
+                    pacRect.X -= pacSpeed;
+                    currentPacTexture = pacLeftTexture;
+                    gametimeSeconds = (float)gameTime.TotalGameTime.TotalSeconds;
+                    foreach (Rectangle barrier in barriers)
+                        if (pacRect.Intersects(barrier))
+                        {
+                            pacRect.X = barrier.Right;
+                        }
+                    if (pacRect.X <= 0)
+                    {
+                        pacRect.X = 0;
+                    }
+                }
+                if (keyboardState.IsKeyDown(Keys.D))
+                {
+                    pacRect.X += pacSpeed;
+                    currentPacTexture = pacRightTexture;
+                    gametimeSeconds = (float)gameTime.TotalGameTime.TotalSeconds;
+                    foreach (Rectangle barrier in barriers)
+                        if (pacRect.Intersects(barrier))
+                        {
+                            pacRect.X = barrier.Left - pacRect.Width;
+                        }
+                    if (pacRect.Right >= _graphics.PreferredBackBufferWidth)
+                    {
+                        pacRect.X = _graphics.PreferredBackBufferWidth - pacRect.Width;
+                    }
+                }
+                if (keyboardState.IsKeyDown(Keys.F) && prevKeyboardState.IsKeyUp(Keys.F))
+                {
+                    if (pacSpeed == 2)
+                    {
+                        pacSpeed = 4;
+                    }
+                    else if (pacSpeed == 4)
+                    {
+                        pacSpeed = 2;
+                    }
+                }
+                if (idleSeconds >= 10)
+                {
+                    currentPacTexture = pacSleepTexture;
+                }
+                for (int i = 0; i < coins.Count; i++)
+                {
+                    if (pacRect.Intersects(coins[i]))
+                    {
+                        coins.RemoveAt(i);
+                        i--;
+                    }
+                }
+                if (keyboardState.IsKeyDown(Keys.E))
+                    if (exitRect.Contains(pacRect))
+                    {
+                        gametimeSeconds = (float)gameTime.TotalGameTime.TotalSeconds;
+                        pacRect = new Rectangle(10, 10, 60, 60);
+                        exitRect = new Rectangle(550, 500, 100, 100);
+                        coins.Clear();
+                        Rectangle newCoin;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            bool done = true;
+                            do
+                            {
+                                done = true;
+                                newCoin = new Rectangle(gen.Next(0, 640 - coinTexture.Width), gen.Next(0, 640 - coinTexture.Height), coinTexture.Width, coinTexture.Height);
+                                if (pacRect.Intersects(newCoin) || exitRect.Intersects(newCoin))
+                                    done = false;
+
+                                foreach (Rectangle coin in coins)
+                                    if (coin.Intersects(newCoin))
+                                        done = false;
+
+
+                                foreach (Rectangle barrier in barriers)
+                                    if (barrier.Intersects(newCoin))
+                                        done = false;
+
+
+
+                            } while (!done);
+                            coins.Add(newCoin);
+                        }
+
+
+                    }
+             
+
+
+                
+                for (int i = 0; i < coins.Count; i++)
+                {
+                    if (coinUpandDown % 2 == 0)
+                    {
+                        if (coinUpandDown < 20)
+                        {
+                            Rectangle coinTemp = new Rectangle();
+                            coinTemp.Width = coins[i].Width;
+                            coinTemp.Height = coins[i].Height;
+                            coinTemp.X = coins[i].X;
+                            coinTemp.Y = coins[i].Y + 1;
+                            coins[i] = coinTemp;
+                            coinUpandDown += 1;
+                        }
+                        else if (coinUpandDown < 40)
+                        {
+                            Rectangle coinTemp = new Rectangle();
+                            coinTemp.Width = coins[i].Width;
+                            coinTemp.Height = coins[i].Height;
+                            coinTemp.X = coins[i].X;
+                            coinTemp.Y = coins[i].Y - 1;
+                            coins[i] = coinTemp;
+                            coinUpandDown += 1;
+                        }
+                        else if (coinUpandDown >= 40)
+                        {
+                            coinUpandDown = 0;
+                        }
+                    }
+                    else
+                        coinUpandDown += 1;
+
+                    
+                }
+            }
 
             base.Update(gameTime);
         }
@@ -97,7 +294,22 @@ namespace Monogame_Lesson_6___Keyboard_and_Mouse_Events
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            if (screen == Screen.intro)
+            {
+                _spriteBatch.Draw(pacmanIntroTexture, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight),Color.White);
+            }
+            else if (screen == Screen.pacman)
+            {
+                foreach (Rectangle barrier in barriers)
+                    _spriteBatch.Draw(barrierTexture, barrier, Color.White);
+                _spriteBatch.Draw(exitTexture, exitRect, Color.White);
+                _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
+                foreach (Rectangle coin in coins)
+                    _spriteBatch.Draw(coinTexture, coin, Color.White);
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
